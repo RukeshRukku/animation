9bdde0c80537d353184289e05fa55cb7a5da077b
RukeshRukku <guditirukesh668@gmail.com>
2024-01-31 13:00:29 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+
+console.log("Rukesh rukku")
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg'>
+      <div className='card'>
+        <form className="login-form-card">
+          <h2 className="login-form-head">Login</h2>
+          <label className="login-form-label" htmlFor="username">Username</label>
+          <input className="login-form-input"
+            id="username" 
+            placeholder="Type Username"
+            type="text"
+            required
+          />
+          <label className="login-form-label" htmlFor="password">Password</label>
+          <input className="login-form-input"
+            id="password" 
+            placeholder="Type Password"
+            type="password"
+            required
+          />
+          <div className="flex">
+            <input type="checkbox" className="checkbox"/>
+            <span className="span">Show Password</span>
+          </div>
+          <input type="submit"
+            className="login-form-btn"
+            value="Login"
+          />
+          <a href="" className="forgot">Forgot Password?</a>
+          <hr className="hr" />
+          <button href="" className="signup">New Register/Sign Up</button>
+        </form>
+      </div>
     </div>
   );
 }
